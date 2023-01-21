cae78d3b8d0d50c0bb6e9df5189c6c467288243a
Christine M <36579842+cm-443@users.noreply.github.com>
2023-01-20 23:31:33 -0500

    Update test

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-import pytz
-from datetime import datetime, timedelta
+import unittest
 def lambda_handler(event, context):
-    est_tz = pytz.timezone('US/Eastern')
-    now = datetime.now(est_tz)
-    timestamp = ""
-    if now.dst():
-       utc_time = datetime.utcnow()
-       utc_time_string_dls = utc_time.strftime('%Y/%m/%d/%H')
-       timestamp = utc_time_string_dls
-       print("It is currently Daylight Saving Time.")
-    else:
-       utc_time = datetime.utcnow()
-       if utc_time.hour == 0:
-           utc_time = utc_time - timedelta(days=1)
-       utc_time = utc_time - timedelta(hours=1)
-       utc_time_string = utc_time.strftime('%Y/%m/%d/%H')
-       timestamp = utc_time_string
-       print("It is currently NOT Daylight Saving Time.")
-       print(timestamp)
+    def fun(x):
+        return x + 1
+
+    class MyTest(unittest.TestCase):
+        def test(self):
+            self.assertEqual(fun(3), 4)
