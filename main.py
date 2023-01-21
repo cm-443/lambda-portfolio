338659042be31488b38faeb566feaf33face6965
Christine M <36579842+cm-443@users.noreply.github.com>
2023-01-20 23:23:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-import datetime
-
+import pytz
+from datetime import datetime, timedelta
 def lambda_handler(event, context):
-    print(datetime.datetime.utcnow() + datetime.timedelta(minutes=30))
-    print("textt")
+    est_tz = pytz.timezone('US/Eastern')
+    now = datetime.now(est_tz)
+    timestamp = ""
+    if now.dst():
+       utc_time = datetime.utcnow()
+       utc_time_string_dls = utc_time.strftime('%Y/%m/%d/%H')
+       timestamp = utc_time_string_dls
+       print("It is currently Daylight Saving Time.")
+    else:
+       utc_time = datetime.utcnow()
+       if utc_time.hour == 0:
+           utc_time = utc_time - timedelta(days=1)
+       utc_time = utc_time - timedelta(hours=1)
+       utc_time_string = utc_time.strftime('%Y/%m/%d/%H')
+       timestamp = utc_time_string
+       print("It is currently NOT Daylight Saving Time.")
+       print(timestamp)
